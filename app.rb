62fe0f1b11308aec10459ff546a3e5825c0ba370
rmaghsoudi <romy_mag@outlook.com>
2019-05-12 18:24:24 -0400

    Donzo

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to the best app ever!!!! I
+    BUILT THIS!, Testing the shotgun!"
   end
 
 end
